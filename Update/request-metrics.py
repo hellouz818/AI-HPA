d521b3bfc33333eaaa5ea57fd652c71c5751bef5
bconfiden2 <bconfiden2@naver.com>
2022-06-26 20:25:36 +0900

    필터링 이후 cpu 사용량 총합

--- Update/request-metrics.py
+++ Update/request-metrics.py
@@ -12,12 +12,16 @@ TOKEN = open(SVCACC + "/token").readline()
 headers = {"Authorization": "Bearer " + TOKEN}
 
 while True:
-    print('---------------------------------------------------------------------------------------------------------------')
-    print(time.ctime()) 
+    print(time.ctime(), end=" : ") 
     pods = json.loads(requests.get(APISERVER + f"/apis/metrics.k8s.io/v1beta1/namespaces/{NS}/pods", verify=SVCACC+"/ca.crt", headers=headers).text)
+    
+    total_cpu_usage = 0
     if 'items' in pods.keys():
         for pod in pods['items']:
             if SCALE_TARGET in pod['metadata']['name']:
-                print(f"{pod['timestamp']}\tname: {pod['metadata']['name']}\tcontainers: {len(pod['containers'])}\tusage: {pod['containers'][0]['usage']}")
-    print('---------------------------------------------------------------------------------------------------------------')
+                cur_usage = pod['containers'][0]['usage']['cpu']
+                if cur_usage[-1] == 'n':
+                    cur_usage = cur_usage[:-1]
+                total_cpu_usage += int(cur_usage)
+    print(total_cpu_usage)
     time.sleep(10)
