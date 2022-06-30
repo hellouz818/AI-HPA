6e89e529bbb115eb6aa06618095960ddb0b58740
bconfiden2 <bconfiden2@naver.com>
2022-06-30 11:58:35 +0900

    utils 와 workflow 구분

---
+++ Update/workflow.py
@@ -0,0 +1,19 @@
+import time
+import os
+from utils import scale, get_metric
+
+SCALE_TARGET = os.environ['SCALE_TARGET']
+
+while True:
+    print(time.ctime(), end=" : ")
+    
+    total_cpu_usage = get_metric(SCALE_TARGET)
+    print(total_cpu_usage, end=", scaled to ")
+    
+    # 모델에 데이터를 보내 replicas 를 받아옴
+
+    replicas = int(time.ctime().split(':')[-1][0])
+    scale(SCALE_TARGET, replicas)
+    print(replicas)
+
+    time.sleep(10)
