2839f0e2d6506d2568063fc939ff2b249de1c425
bconfiden2 <bconfiden2@naver.com>
2022-06-30 10:51:37 +0900

    Scale API 테스트

---
+++ Update/deployment-scale.py
@@ -0,0 +1,21 @@
+import requests
+import time
+import json
+import os
+
+SCALE_TARGET = os.environ['SCALE_TARGET']
+
+APISERVER = "https://kubernetes.default.svc"
+SVCACC = "/var/run/secrets/kubernetes.io/serviceaccount"
+NS = open(SVCACC + "/namespace").readline()
+TOKEN = open(SVCACC + "/token").readline()
+
+def scale(replica=1):
+    URL = APISERVER + "/apis/apps/v1/namespaces/" + NS + "/deployments/" + SCALE_TARGET
+    PAYLOAD = "{\"spec\":{\"replicas\":" + str(replica) + "}}"
+    HEADER = {"Authorization": "Bearer " + TOKEN, "Content-Type": "application/strategic-merge-patch+json"}
+    requests.patch(URL, PAYLOAD, headers=HEADER, verify=SVCACC+"/ca.crt"
+
+while True:
+    scale(int(time.ctime().split(':')[-1][0]))
+    time.sleep(10)
